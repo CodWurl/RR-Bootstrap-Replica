66ae6df60d0ab1d273ecb414906990b737b5905d
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-08-24 17:56:10 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+import instaCartLogo from './logo.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Navbar bg="light" expand="lg" >
+    <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+        <img src={instaCartLogo} alt="Logo image" /> 
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"> 
+        </Nav>
+        <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>{' '}
+        </Nav>
+    </Navbar.Collapse>
+</Navbar>
   );
-}
+
+  }
 
 export default App;
